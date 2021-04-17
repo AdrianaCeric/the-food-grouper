c74fa9f343d5864ce4ce1d6b90e586b0886c6e53
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2021-04-17 19:34:32 -0400

    final code

--- the-food-grouper/js/predict.js
+++ the-food-grouper/js/predict.js
@@ -1,22 +1,21 @@
 // the link to model provided by Teachable Machine export panel
-// a variable (var) will be able to be reused throughout the whole program
-// declaring variable var url and assigning variable url to link (= is assignment operator)
 var url = "https://teachablemachine.withgoogle.com/models/nHLwAj9Zo/"
 
-//To help manipulate the image and the file upload, save those two DOM elements into some variables.
 // make a variable "input" that retrieves id image-selector
 var inp = document.getElementById("image-selector");
+// make variable "upload" that retrieves class upload-button-wrapper
+
+var upl = document.getElementsByClassName("upload-btn-wrapper")[0];
 
 // Drag and drop
-// male variable "upload" that retrieves class upload-button-wrapper
-var upl = document.getElementsByClassName("upload-btn-wrapper")[0];
+
 //allowing event dragover to happen (Prevent file from being opened)
 upl.addEventListener("dragover", function(event) {
+    //Prevent the default handling of the element.
     event.preventDefault();
 })
 
 upl.addEventListener("drop", function(event) {
-    //By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element.
     event.preventDefault();
     // DataTransfer object is used to hold the data that is being dragged during a drag and drop operation
     inp.files = event.dataTransfer.files;
@@ -27,10 +26,9 @@ upl.addEventListener("drop", function(event) {
         // A Data URL is a URI scheme that provides a way to inline data in an HTML document.
         //returns file on screen
         let dataURL = reader.result;
-        //what to show when file is uploaded (button and image w/ container)
-        //The attr() method sets or returns attributes and values of the selected elements. 
-        //When this method is used to set attribute values, it sets attribute (src) and value pairs (dataURL) for the set of matched elements.
+        //Set the source attribute of the selected image to the value of dataURL.
         $("#selected-image").attr("src", dataURL);
+        //what to show when file is uploaded (button and image w/ container)
         $("#prediction-list").empty();
         $("#explanation-list").empty();
         $("#predict-button").show();
@@ -38,9 +36,7 @@ upl.addEventListener("drop", function(event) {
         $(".upload-btn-wrapper").show();
         $(".predict").hide();
     }
-    // let can only be used within the scope of where you declare this
-    //The FileReader is asynchronous. We have to wait for the result with onload before we can display the image
-    //we get the selected file from the image-selector, and load the image by calling readAsDataURL on reader and passing in the selected image file.
+    
     let file = $("#image-selector").prop('files')[0];
     reader.readAsDataURL(file);
     $(".predict-btn-wrapper").show()
@@ -48,6 +44,7 @@ upl.addEventListener("drop", function(event) {
 
 //upload file
 $(async function() {
+    // predict button and image container only appear after image is loaded
     await $("#predict-button").hide();
     await $(".image-container").hide();
     $("#image-selector").change(function() {
@@ -56,7 +53,7 @@ $(async function() {
         reader.onload = function() {
             let dataURL = reader.result;
             $("#selected-image").attr("src", dataURL);
-            //get rid of any previous predictions that were being displayed for previous images. 
+            //Line 55: get rid of any previous predictions that were being displayed for previous images. 
             $("#prediction-list").empty();
             $("#explanation-list").empty();
             $(".confirmation-text").hide();
@@ -72,10 +69,8 @@ $(async function() {
     });
 
     // load the model and metadata from Teachable Machine
-    // tf.loadLayersModel() returns a Promise. Meaning that this function promises to return the model at some point in the future.
 
-
-    //This await key word pauses the execution of this wrapping function until the promise is resolved and the model is loaded. This is why we use the async keyword when defining this function... Because if we want to use the await keyword, then it has to be contained within an async function.
+    //This await key word pauses the execution of this function until the promise is resolved and the model is loaded. 
     model = await tmImage.load(url + "model.json", url + "metadata.json")
     $(".progress").hide();
     maxPredictions = model.getTotalClasses();
@@ -84,39 +79,36 @@ $(async function() {
         $(".predict-btn-wrapper").hide()
         //show prediction on screen
         $(".predict").show();
-        /*$(".progress").html("<h5>Classifying food...</h5>");*/
         var prediction = await model.predict(document.getElementById("selected-image"));
         var fails = 0;
-        //Print classification on screen if prediction is equal or more than 60% accurate
-        // View prediction in console
+        // View prediction on console
         console.log(prediction)
+        //for loop
+        //Print classification (executing setP function) on screen if prediction is equal or more than 60% accurate
         for (var i = 0; i < prediction.length; i++) {
             console.log(prediction[i.toString()]);
             if (prediction[i.toString()].probability > 0.6) {
-                console.log("ITS THISSSS");
+                console.log("Prediction Valid");
 
                 if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "M") {
-                    setP("<p>Main Course</p>", 0);
+                    setP("<p>Main Course</p>");
                 } else if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "A") {
-                    setP("<p>Appetizer</p>", 1);
+                    setP("<p>Appetizer</p>");
                 } else if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "D") {
-                    setP("<p>Dessert</p>", 2);
+                    setP("<p>Dessert</p>");
                 } 
                 
-            } else {
-                fails++;
-            }
+            } 
         }
         //Runtime error handling
         console.log(fails)
-        if (fails == 4) {
+        if (fails == 1) {
             setP("Unable to identify. Please try again with another photo.");
         }
-        window.scrollTo(0, document.body.scrollHeight);
     })
 })
 
-
+//Function to show correct proediction
 function setP(a) {
     $(".upload-btn-wrapper").show();
     $("#prediction-list").html(a);
--- the-food-grouper/js/target_classes.js
+++ the-food-grouper/js/target_classes.js
@@ -1,5 +1,5 @@
-TARGET_CLASSES = {
+/*TARGET_CLASSES = {
     0: "Appetizer",
     1: "Main Course",
     2: "Dessert",
-};
+};*/
