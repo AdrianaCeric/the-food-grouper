76bdbeca02975bda53455eb5766df5e5a050b618
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2021-04-16 23:30:47 -0400

    comments

--- the-food-grouper/js/predict.js
+++ the-food-grouper/js/predict.js
@@ -1,24 +1,32 @@
 // the link to model provided by Teachable Machine export panel
+// a variable (var) will be able to be reused throughout the whole program
+// declaring variable var url and assigning variable url to link (= is assignment operator)
 var url = "https://teachablemachine.withgoogle.com/models/nHLwAj9Zo/"
 
-
+// make a variable "input" that retrieves id image-selector
 var inp = document.getElementById("image-selector");
+
+// Drag and drop
+// male variable "upload" that retrieves class upload-button-wrapper
 var upl = document.getElementsByClassName("upload-btn-wrapper")[0];
 upl.addEventListener("dragover", function(event) {
     event.preventDefault();
 })
 
-// Drag and drop
+
 upl.addEventListener("drop", function(event) {
     //By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element.
     event.preventDefault();
     inp.files = event.dataTransfer.files;
     // access the contents of files that the user has explicitly selected
     let reader = new FileReader();
+    //when loads
     reader.onload = function() {
         //returns file on screen
         let dataURL = reader.result;
         //what to show when file is uploaded (button and image w/ container)
+        //The attr() method sets or returns attributes and values of the selected elements. 
+        //When this method is used to set attribute values, it sets attribute (src) and value pairs (dataURL) for the set of matched elements.
         $("#selected-image").attr("src", dataURL);
         $("#prediction-list").empty();
         $("#explanation-list").empty();
@@ -27,7 +35,7 @@ upl.addEventListener("drop", function(event) {
         $(".upload-btn-wrapper").show();
         $(".predict").hide();
     }
-
+    // let can only be used within the scope of where you declare this
     let file = $("#image-selector").prop('files')[0];
     reader.readAsDataURL(file);
     $(".predict-btn-wrapper").show()
@@ -38,7 +46,7 @@ $(async function() {
     await $("#predict-button").hide();
     await $(".image-container").hide();
     $("#image-selector").change(function() {
-        // $(".upload-btn-wrapper").hide();
+        $(".upload-btn-wrapper").hide();
         let reader = new FileReader();
         reader.onload = function() {
             let dataURL = reader.result;
@@ -68,8 +76,8 @@ $(async function() {
         /*$(".progress").html("<h5>Classifying food...</h5>");*/
         var prediction = await model.predict(document.getElementById("selected-image"));
         var fails = 0;
-
         //Print classification on screen if prediction is equal or more than 60% accurate
+        // View prediction in console
         console.log(prediction)
         for (var i = 0; i < prediction.length; i++) {
             console.log(prediction[i.toString()]);
