417336bb5dc6e8b4992a9f4decf9f9537217a8c1
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2021-04-18 11:19:30 -0400

    ok

--- the-food-grouper/js/predict.js
+++ the-food-grouper/js/predict.js
@@ -1,32 +1,29 @@
 // the link to model provided by Teachable Machine export panel
 var url = "https://teachablemachine.withgoogle.com/models/nHLwAj9Zo/"
 
-// make a variable "input" that retrieves id image-selector
+// two variables to help manipulate image and file upload
 var inp = document.getElementById("image-selector");
-// make variable "upload" that retrieves class upload-button-wrapper
 
 var upl = document.getElementsByClassName("upload-btn-wrapper")[0];
 
 // Drag and drop
 
-//allowing event dragover to happen (Prevent file from being opened)
+//drag function
 upl.addEventListener("dragover", function(event) {
-    //Prevent the default handling of the element.
+    //allowing event dragover to happen (Prevent file from being opened)
     event.preventDefault();
 })
 
+//drop function
 upl.addEventListener("drop", function(event) {
     event.preventDefault();
-    // DataTransfer object is used to hold the data that is being dragged during a drag and drop operation
+    // DataTransfer object to hold the data that is being dragged
     inp.files = event.dataTransfer.files;
-    // access the contents of files that the user has explicitly selected
+    // read file contents
     let reader = new FileReader();
-    //when loads
+    //when loads function
     reader.onload = function() {
-        // A Data URL is a URI scheme that provides a way to inline data in an HTML document.
-        //returns file on screen
         let dataURL = reader.result;
-        //Set the source attribute of the selected image to the value of dataURL.
         $("#selected-image").attr("src", dataURL);
         //what to show when file is uploaded (button and image w/ container)
         $("#prediction-list").empty();
@@ -36,13 +33,13 @@ upl.addEventListener("drop", function(event) {
         $(".upload-btn-wrapper").show();
         $(".predict").hide();
     }
-    
+    //show image on screen
     let file = $("#image-selector").prop('files')[0];
     reader.readAsDataURL(file);
     $(".predict-btn-wrapper").show()
 })
 
-//upload file
+//upload file function
 $(async function() {
     // predict button and image container only appear after image is loaded
     await $("#predict-button").hide();
@@ -52,8 +49,8 @@ $(async function() {
         let reader = new FileReader();
         reader.onload = function() {
             let dataURL = reader.result;
-            $("#selected-image").attr("src", dataURL);
-            //Line 55: get rid of any previous predictions that were being displayed for previous images. 
+            $("#selected-image").attr("src", dataURL); 
+            //Get rid of any previous predictions that were being displayed for previous images.
             $("#prediction-list").empty();
             $("#explanation-list").empty();
             $(".confirmation-text").hide();
@@ -61,8 +58,6 @@ $(async function() {
             $(".image-container").show();
             $(".predict").hide();
         }
-        //Let selector image-selector return property "files"
-        // The FileReader is asynchronous. We have to wait for the result with onload before we can display the image.
         let file = $("#image-selector").prop('files')[0];
         reader.readAsDataURL(file);
         $(".predict-btn-wrapper").show();
@@ -70,9 +65,10 @@ $(async function() {
 
     // load the model and metadata from Teachable Machine
 
-    //This await key word pauses the execution of this function until the promise is resolved and the model is loaded. 
+    //Await key word pauses the execution of this function until the promise is resolved and the model is loaded. 
     model = await tmImage.load(url + "model.json", url + "metadata.json")
     $(".progress").hide();
+    //this line of code from https://teachablemachine.withgoogle.com/ to make prediction
     maxPredictions = model.getTotalClasses();
 
     $("#predict-button").click(async function() {
@@ -81,13 +77,13 @@ $(async function() {
         $(".predict").show();
         var prediction = await model.predict(document.getElementById("selected-image"));
         var fails = 0;
-        // View prediction on console
         console.log(prediction)
         //for loop
         //Print classification (executing setP function) on screen if prediction is equal or more than 60% accurate
         for (var i = 0; i < prediction.length; i++) {
             console.log(prediction[i.toString()]);
             if (prediction[i.toString()].probability > 0.6) {
+                //Test if there is prediction
                 console.log("Prediction Valid");
 
                 if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "M") {
@@ -110,7 +106,7 @@ $(async function() {
     })
 })
 
-//Function to show correct proediction
+//Function to show final prediction
 function setP(a) {
     $(".upload-btn-wrapper").show();
     $("#prediction-list").html(a);
